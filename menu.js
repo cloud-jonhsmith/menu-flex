78fbef85512e099a6dfef4bbba1650ca10ace608
Jonhsmith Cordova <jonhsmith.cordova@gmail.com>
2020-08-28 16:21:18 -0500

    Proyecto terminado

--- menu.js
+++ menu.js
@@ -1,9 +1,25 @@
+/* controla el hamburger al hacer click */
 var menu = document.getElementById("menu-movil")
+
+/* controla el cross al cerrar el menú movil */
 var cross = document.getElementById("cross")
+
+/* para activar o desactivar la vista de los items del menú movil */
 var items = document.getElementById("nav-menu")
+
+/* controla los items del menú de navegación */
 var item1 = document.getElementById("item1")
 var item2 = document.getElementById("item2")
 var item3 = document.getElementById("item3")
+
+/* cuando se abre el menú movil, se oculta el contenido */
+var cajas = document.getElementById("cajas")
+
+/* se usará para variar el height y abarcar toda la pantalla del movil */
+var container = document.getElementById("container")
+
+/* abarcará el 100% del container */
+var nav = document.getElementById("nav-container")
 
 menu.addEventListener("click", function(){
     menu.style.display="none"
@@ -15,6 +31,11 @@ menu.addEventListener("click", function(){
     item1.style.display="block"
     item2.style.display="block"
     item3.style.display="block"
+
+    cajas.style.display="none"
+
+    container.style.height="1080px"
+    nav.style.height="100%"
 })
 
 cross.addEventListener("click", function(){
